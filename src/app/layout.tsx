b31fd0f3a7656de7553137a6f37e8af65adfb6fe
Trevin Avery <TrevinAvery@gmail.com>
2024-07-12 11:57:00 -0700

    Setup MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter'
+import { ThemeProvider } from '@mui/material/styles'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
+import theme from '../theme'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -17,7 +17,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body>
+				<AppRouterCacheProvider>
+					<ThemeProvider theme={theme}>{children}</ThemeProvider>
+				</AppRouterCacheProvider>
+			</body>
 		</html>
 	)
 }
