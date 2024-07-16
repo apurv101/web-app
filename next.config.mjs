c6db9a54c16e464be4e0c122685c079a4d5a282c
Trevin Avery <TrevinAvery@gmail.com>
2024-07-15 19:47:06 -0700

    Make static and add pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	output: 'export',
+
+	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+	// trailingSlash: true,
+
+	// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+	// skipTrailingSlashRedirect: true,
+
+	// Optional: Change the output directory `out` -> `dist`
+	distDir: 'dist',
+
+	// temporarily disable optimization until we create a custom loader
+	// https://nextjs.org/docs/app/building-your-application/deploying/static-exports#image-optimization
+	images: { unoptimized: true },
+}
 
 export default nextConfig
