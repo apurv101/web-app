973346db0442a382135bb42edc5ad53355badcce
Trevin Avery <TrevinAvery@gmail.com>
2024-08-28 10:06:58 -0700

    Add env vars and beta run target

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,16 @@
+// @ts-check
+import webpack from 'webpack'
+
+const FRONTEND_ENV_KEYS = ['NODE_ENV', 'API_HOSTNAME_OVERRIDE']
+
+const envPlugin = FRONTEND_ENV_KEYS.reduce(
+	(result, key) =>
+		Object.assign({}, result, {
+			[`process.env.${key}`]: JSON.stringify(process.env[key]),
+		}),
+	{},
+)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
@@ -14,6 +27,12 @@ const nextConfig = {
 	// temporarily disable optimization until we create a custom loader
 	// https://nextjs.org/docs/app/building-your-application/deploying/static-exports#image-optimization
 	images: { unoptimized: true },
+
+	webpack: (config, _context) => {
+		config.plugins.push(new webpack.DefinePlugin(envPlugin))
+		// Important: return the modified config
+		return config
+	},
 }
 
 export default nextConfig
