969a8e2ae5a5a0adc2f1f883011b887ce65ef219
Trevin Avery <TrevinAvery@gmail.com>
2024-10-01 17:15:32 -0700

    Use default distDir in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,20 +14,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	// output: 'export',
-
 	// Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
 	// trailingSlash: true,
-
 	// Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
 	// skipTrailingSlashRedirect: true,
-
 	// Optional: Change the output directory `out` -> `dist`
-	distDir: 'assets/dist',
-
+	// distDir: 'assets/dist',
 	// temporarily disable optimization until we create a custom loader
 	// https://nextjs.org/docs/app/building-your-application/deploying/static-exports#image-optimization
 	// images: { unoptimized: true },
-
 	// webpack: (config, _context) => {
 	// 	config.plugins.push(new webpack.DefinePlugin(envPlugin))
 	// 	// Important: return the modified config
